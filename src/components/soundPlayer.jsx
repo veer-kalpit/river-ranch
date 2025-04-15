8a76909e9b8d3cbb6502ed48ba56960f899f2308
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2025-04-16 00:00:40 +0530

    updates

--- src/components/soundPlayer.jsx
+++ src/components/soundPlayer.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 function SoundPlayer({ isRendered }) {
   const playClickSound = () => {
     const audio = new Audio("/sounds/river.mp3");
     audio.loop = true;
-    audio.play();
+    audio.play().catch((err) => console.error("Audio play failed:", err));
   };
 
   useEffect(() => {
     if (!isRendered) return;
     playClickSound();
   }, [isRendered]);
+
+  return null;
 }
 
 export default SoundPlayer;
