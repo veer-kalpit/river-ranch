25cf3f7f32df2d47b63c051514eee63ca19047e6
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2025-04-30 21:46:45 +0530

    updates

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,7 +1,8 @@
 // lib/mongodb.ts
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI =
+  "mongodb+srv://admin:riverRanch@cluster0.n5kna6e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
 
